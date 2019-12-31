0bc755c05839a03eb3e332167951d302c987b441
Will Parks <wparks08@gmail.com>
2019-12-30 23:51:42 -0800

    finish off burger controller put and post methods

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,23 +4,29 @@ const Burger = require("../models/burger");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Burger.selectAll()
-        .then(burgers => {
-            res.render("index", { burgers })
-        }) 
-})
-
-// router.get("/all", (req, res) => {
-//     //list
-//     res.render("")
-// });
-
-router.post("/burger", (req, res) => {
-    //insert
+    Burger.selectAll().then(burgers => {
+        res.render("index", { burgers });
+    });
 });
 
-router.put("/burger", (req, res) => {
-    //update
-})
+router.post("/api/burger", (req, res) => {
+    let burger = req.body;
+
+    Burger.insertOne(burger);
+    
+    res.redirect("/");
+});
+
+router.put("/api/burger/:id", (req, res) => {
+    let burger = req.body;
+
+    if (!burger.id) {
+        res.redirect("/");
+        return;
+    }
+
+    Burger.updateOne(burger, { id: req.params.id });
+    res.redirect("/");
+});
 
 module.exports = router;
